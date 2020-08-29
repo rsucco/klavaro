ada57f2fb59bda74aeef8d222056471a617021d2
Ryan Succo <rsucco@live.com>
2020-08-28 22:22:59 -0400

    Initial Commit

---
+++ klavaro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  klavaro
+//
+//  Created by Ryan Succo on 8/28/20.
+//  Copyright © 2020 Ryan Succo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
